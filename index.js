923e76bc14cbedd00858dd940cfa336661a56e46
codeology <coder451@codeology.co.nz>
2015-11-10 15:27:21 +1300

    new site

--- index.js
+++ index.js
@@ -2,17 +2,25 @@
 
     var express = require('express');
     var app = express();
-
+    var bodyParser = require('body-parser');
 
     app.set('views', './public/tmpl');
     app.set('view engine', 'jade');
     app.engine('jade', require('jade').__express);
     app.use(express.static(__dirname + '/public'));
 
+    app.use(bodyParser.json()); // for parsing application/json
+    app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 
     app.get('/', function(req, res){
         res.render("homepage")
+    });
+
+
+    app.post('/api/contact', function(req, res){
+        console.log(">x>x>x>x>", req.body)
+        res.json( req.query );
     });
 
 
