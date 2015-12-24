a0eb1f7691aa9ca5d63f90dbc8c01c6bf98b174c
codeology <coder451@codeology.co.nz>
2015-12-24 13:31:53 +1300

    oops

--- index.js
+++ index.js
@@ -37,15 +37,6 @@
         res.json(email);
 
     });
-    //
-    //app.get('/lab/twopic', require("./lab/twopic"));
-    //
-    //
-    //app.post('/api/nlp/tokenize/sentences', require("./api/nlp/tokenizeSentences"));
-    //app.get('/api', function( req, res ){
-    //    res.render("api");
-    //});
-
 
 
     app.listen(process.env.PORT || 3000, function(){
